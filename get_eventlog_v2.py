470c97df2f1555c5b58d19075c70b2b3486c7286
Demetrius Storm <47479409+DemetriusStorm@users.noreply.github.com>
2019-11-19 13:50:58 +0300

    Update get_eventlog_v2.py

--- get_eventlog_v2.py
+++ get_eventlog_v2.py
@@ -1,20 +1,8 @@
 from winevt import EventLog
 
-SystemTime = "event.System.TimeCreated['SystemTime']"
-PrintServer = "event.System.Computer"
-Document = "event.UserData.Param2"
-Username = "event.UserData.Param3"
-Computer = "event.UserData.Param5"
-Count = "event.UserData.Param8"
 
-query = EventLog.Query("Microsoft-Windows-PrintService/Operational", "Event/System[EventID=307]")
+query = EventLog.Query("Microsoft-Windows-PrintService/Operational", "Event/System[EventID=307]", handle_event)
 
 for event in query:
-    print(event.xml())
-
-    # print(event.System.TimeCreated['SystemTime'],
-    #       event.System.Computer)
-          # event.UserData.DocumentPrinted.Param3[0],
-          # event.UserData.DocumentPrinted.Param5,
-          # event.UserData.DocumentPrinted.Param8)
+    print("Got event: " + str(event))
 
